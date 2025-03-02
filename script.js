a467ef3f14ad6b686dea5b542f68bfe74a002b4e
Alone Mpitula <Alonemapitlula@gmail.com>
2025-03-02 14:16:04 +0200

    index.html

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
+function toggleMenu()
+ {
+  //querySelector initiate the screen i gues
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
