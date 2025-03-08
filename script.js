c9309b66ba44f2d8e39151a0473eb9a8c9b2299e
Alone Mpitula <Alonemapitlula@gmail.com>
2025-03-08 16:53:57 +0200

    js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function toggleMenu()
- {
-  //querySelector initiate the screen i gues
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+function toggleMenu() {
+  //querySelector initiate the screen
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
